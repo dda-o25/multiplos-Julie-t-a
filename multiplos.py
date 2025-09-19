63e0571a62bf588034cbe48be72253e49a241f40
Julie-t-a <julieta.nunez@iteso.mx>
2025-09-19 01:13:22 -0600

    primer commit

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,22 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Julieta Núñez Pacheco
+19 de septiembre de 2025
+Este ejercicio indica si un número es múltiplo de otro
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+num1 = int(input("un numero: "))
+num2 = int(input("otro numero: "))
 
 # Proceso
 
+if num1 % num2 == 0:
+    multiplo = str(num1) + " es multiplo de " + str(num2)
+elif num2 % num1 == 0:
+    multiplo = str(num2) + " es multiplo de " + str(num1)
+else: 
+    multiplo = "Ninguno de los números es múltiplo del otro"
+
 
 # Salidas
-print(salida)
+print(multiplo)
